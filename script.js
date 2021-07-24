d80f70f5cacc38317709f890511214f8cf7ca861
Esther-Moki <esther.moki@student.moringaschool.com>
2021-07-24 18:52:52 +0300

    added padding to the hover part

--- script.js
+++ script.js
@@ -30,48 +30,85 @@ $(document).ready(function(){
  });
 });
 
-
-
-
-
-
 //end of the function//
 
-
-// $(document).ready(function() {
-  
-//   $(".empty1").mouseenter  (function(){
-//    $(".overlay").css("opacity","0.1");
-//   });
-  
-//   $(".empty1").mouseleave(function(){
-//    $(".overlay").css("opacity","0");
-//      });
-  
-//   });
-
+//beginning of jquery hover effect on the portfolio part//
 $(document).ready(function() {
-  
-  $(".firstpart").mouseenter  (function(){
+  $(".empty1").mouseenter(function(){
+   $(".overlayone").css("opacity","0.3");
+  });
+  $(".empty1").mouseleave(function(){
+   $(".overlayone").css("opacity","0");
+     });
+});
+$(document).ready(function() {
+  $(".empty2").mouseenter(function(){
+   $(".overlaytwo").css("opacity","0.7");
+  });
+  $(".empty2").mouseleave(function(){
+   $(".overlaytwo").css("opacity","0");
+     });
+});
+$(document).ready(function() {
+  $(".empty3").mouseenter(function(){
+   $(".overlaythree").css("opacity","0.4");
+  });
+  $(".empty3").mouseleave(function(){
+   $(".overlaythree").css("opacity","0");
+     });
+});
+$(document).ready(function() {
+  $(".empty4").mouseenter(function(){
+   $(".overlayfour").css("opacity","0.5");
+  });
+  $(".empty4").mouseleave(function(){
+   $(".overlayfour").css("opacity","0");
+     });
+});
+$(document).ready(function() {
+  $(".firstpart").mouseenter(function(){
    $(".overlayfive").css("opacity","0.6");
   });
-  
   $(".firstpart").mouseleave(function(){
    $(".overlayfive").css("opacity","0");
      });
-  
-  });
+});
   $(document).ready(function() {
-  
     $(".secondpart").mouseenter  (function(){
      $(".overlaysix").css("opacity","0.6");
     });
-    
     $(".secondpart").mouseleave(function(){
      $(".overlaysix").css("opacity","0");
        });
-    
-    });
+});
+$(document).ready(function() {
+  $(".thirdpart").mouseenter  (function(){
+   $(".overlayseven").css("opacity","0.6");
+  });
+  $(".thirdpart").mouseleave(function(){
+   $(".overlayseven").css("opacity","0");
+     });
+});
+$(document).ready(function() {
+  $(".fourthpart").mouseenter  (function(){
+   $(".overlayeight").css("opacity","0.6");
+  });
+  $(".fourthpart").mouseleave(function(){
+   $(".overlayeight").css("opacity","0");
+     });
+});
+
+
+//end of jquery hover effect on the portfolio part//
+
+
+
+
+
+
+
+
+
 
 
 // $(document).ready(function(){
